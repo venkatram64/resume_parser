01abd465468267a7ac8fd93ab2882424a2696363
venkatram64 <venkat.veerareddy@hotmail.com>
2019-02-08 11:12:16 +0530

    init commit

---
+++ src/main/java/com/venkat/resumeparser/ResumeParserApplication.java
@@ -0,0 +1,14 @@
+package com.venkat.resumeparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeParserApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/venkat/resumeparser/model/Type.java
@@ -0,0 +1,19 @@
+package com.venkat.resumeparser.model;
+
+public enum Type {
+    PERSON("Person"),
+    CITY("City"),
+    STATE_OR_PROVINCE("State_or_Province"),
+    COUNTRY("Country"),
+    EMAIL("Email"),
+    TITLE("Title");
+
+    private String type;
+    Type(String type){
+        this.type = type;
+    }
+
+    public String getName(){
+        return this.type;
+    }
+}
